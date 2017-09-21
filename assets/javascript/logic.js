f066a9f3b4dab92f4bca184fec2373a31a09c084
Jachaplin <jakealanchaplin@yahoo.com>
2017-09-20 17:11:32 -0700

    Working App

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,63 +1,92 @@
 $(document).ready(function() {
-
 // Public Giphy API Key  dc6zaTOxFJmzC
-
-// -###### Parameters
-//  -
-//  -+ q - search query term or phrase
-//  -+ limit - (optional) number of results to return, maximum 100. Default 25.
-//  -+ offset - (optional) results offset, defaults to 0.
-//  -+ rating - (optional) limit results to those rated (y,g, pg, pg-13 or r).
-//  -+ lang - (optional) specify default country for regional content; format is 2-letter ISO 639-1 country code. See list of supported languages [here](#language-support)
-//  -+ fmt - (optional) return results in html or json format (useful for viewing responses as GIFs to debug/test)
-//  -+ sort - (optional) the sort order of the results returned (recent | relevant)
-
- // switch the protocol in the query URL from http to https
+// switch the protocol in the query URL from http to https
 
 
+// Default buttons
   var topics = ["Alfa Romeo", "Aston Martin", "Audi", "Bugatti", "Ferrari", "Jaguar", "Lamborghini", "Lotus", "McLaren", "Maserati"]
 
+// makes request to giphy and displays 10 giffs or images when button is pressed
   	function displayCarGif() {
-  	  $("#car-gifs").empty()
-  	  var carGif = $(this).attr("data-name")	
-  	  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + carGif + "&api_key=dc6zaTOxFJmzC&limit=10&rating"
+  	  
+      $("#car-gifs").empty()
+  	  var cars = $(this).attr("data-name")	
+  	  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + cars + "&api_key=dc6zaTOxFJmzC&limit=10"
 
+// ajax get request
       $.ajax({
         url: queryURL,
         method: "GET"
       }).done(function(response) {
-        console.log(response)
-        console.log(response.data["0"].embed_url)
 
+        var results = response.data
+
+        // Looping through the get request results
         for (var i = 0; i < 10; i++) {
-        	
+          
+          // images of the 10 results
+          var imageURL = results[i].images.original_still.url
+          
+          // gifs of the 10 results
+          var gifURL = results[i].images.downsized.url
+                   
+          // ratings of the 10 results
+          var rating = results[i].rating
+          
+          // image tag to hold the results
+          var carImgTag = $("<img>")
 
-          var gifURL = response.data[i].embed_url
-          var addCarGif = $("<iframe>")
-          addCarGif.attr("src", gifURL)
-          addCarGif.attr("alt", "car gif")
-          $("#car-gifs").prepend(addCarGif)
+          // adding attr to the image results
+          carImgTag.attr("src", imageURL)
+          
+          carImgTag.attr("data-still", imageURL)
 
-        // var carDiv = $("<div class='cars'>")
+          carImgTag.attr("data-animate", gifURL)
+
+          var currentState = carImgTag.attr("data-state", "still")
+          
+          carImgTag.addClass("gif")
+          
+          var p = $("<p>").text("Rating: " + rating)
+          
+          var carDiv = $("<div>")
+
+          carImgTag.append(currentState)
+          carDiv.append(carImgTag)
+          carDiv.append(p)
+          
+          $("#car-gifs").prepend(carDiv)
+           
         }
-      
       })
-  	}
+    }
 
-  	function renderButtons() {
+    function renderButtons() {
       
       $("#car-buttons").empty()
 
       topics.forEach(function(car) {
-     	// console.log(car)
-     	oneCar = $("<button>")
-     	oneCar.addClass("cars")
-     	oneCar.attr("data-name", car)
-     	oneCar.text(car)
-     	$("#car-buttons").append(oneCar)
+      
+        oneCar = $("<button>")
+        oneCar.addClass("car-button-class")
+        oneCar.attr("data-name", car)
+        oneCar.text(car)
+        $("#car-buttons").append(oneCar)
      
      })
-  	}
+    }
+          $("body").on("click", ".gif", function() {
+   
+            if ($(this).attr("data-state") === "still") {
+    
+                $(this).attr("src", $(this).attr("data-animate"))
+                $(this).attr("data-state", "animate")
+            }
+            else {
+                $(this).attr("data-state", "still")
+                $(this).attr("src", $(this).attr("data-still"))
+            }
+        })
 
   	$("#add-car").on("click", function(event) {
   		
@@ -67,11 +96,8 @@ $(document).ready(function() {
   		renderButtons()
   		$("#car-input").val("")
   	})
-  $(document).on("click", ".cars", displayCarGif)   
+  
+  $(document).on("click", ".car-button-class", displayCarGif)   
   renderButtons()  
-  // displayCarGif()
-
-
-
 
 })
